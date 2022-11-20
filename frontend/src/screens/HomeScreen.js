30cf51120028f8800255afead9fe0c4c7cb4d972
Andres <andresfgp1118@gmail.com>
2022-11-19 23:12:44 -0500

    Tercer commit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,69 +1,70 @@
+import { useEffect, useReducer } from "react";
 
-import { useEffect, useReducer} from "react";
-import { Link } from 'react-router-dom';
-import React from 'react';
-import axios from 'axios';
-import logger from 'use-reducer-logger';
+import React from "react";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 //import data from '../data';
 
-
 const reducer = (state, action) => {
-    switch(action.type) {
-        case 'FETCH_REQUEST':
-            return {...state, loading: true};
-        case 'FETCH_SUCCESS':
-            return {...state, products: action.payload, loading: false};
-        case 'FETCH_FAIL':
-            return {...state, loading: false, error: action.payload};
-        default:
-            return state; 
-    }
-}
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function HomeScreen() {
-    const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
-        products: [],
-        loading: true, error: '',
-    });
-    //const [products, setProducts] = useState([]);
-    useEffect(() => {
-        const fecthData = async () => {
-            dispatch({type: 'FETCH_REQUEST'});
-            try {
-                const result = await axios.get('http://localhost:5000/api/products')
-                dispatch({type: 'FETCH_SUCCESS', payload: result.data });
-            } catch (err) {
-                dispatch({type: 'FETCH_FAIL', payload: err.message});
-        }
-        };
-        fecthData();
-    }, []);
-    return (
-        <div>
-            <h1> Lista de Gastos</h1>
-            <div className="products"> 
-            {loading ? (
-                    <div> Cargando Datos </div>
-                ) : error ? ( 
-                    <div>{error}</div>
-                ) : (
-            products.map((product) => (
-            <div className="product" key={product.slug}>
-                <Link to= {`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name}  />
-                </Link>
-                <div className="product-info">
-                <a href= {`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                </a>
-                    <p>{product.price}</p>
-                <button> Agregar Gasto</button>
-                </div>  
-            </div>))
-            )}
-            </div>
-        </div>
-    )
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  //const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fecthData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("http://localhost:5000/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+    };
+    fecthData();
+  }, []);
+  return (
+    <div>
+      <Helmet>
+        <title>Registro de Gastos 2$$$</title>
+      </Helmet>
+      <h1> Lista de Gastos</h1>
+      <div className="products">
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default HomeScreen;
